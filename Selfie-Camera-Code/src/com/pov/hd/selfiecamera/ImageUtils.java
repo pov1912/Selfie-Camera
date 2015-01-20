92237309d2402861db1e7f7e7cc86562fbd736e0
pov1912 <pov.12192009@gmail.com>
2015-01-20 20:20:43 +0700

    Update ImageUtils.java

--- Selfie-Camera-Code/src/com/pov/hd/selfiecamera/ImageUtils.java
+++ Selfie-Camera-Code/src/com/pov/hd/selfiecamera/ImageUtils.java
@@ -25,8 +25,6 @@ import android.view.View.MeasureSpec;
  * */
 public class ImageUtils {
 
-	public static final String FOLDER_IMG = "Romantic_PhotoEditor";
-
 	/**
 	 * This method used for create file name from system date time.
 	 * 
@@ -80,101 +78,5 @@ public class ImageUtils {
 	 * */
 	private static final String TAG = ImageUtils.class.getSimpleName();
 
-	public static int getPxFromDip(float valueDip, Context context) {
-
-		// Get the screen's density scale.
-		float scale = context.getResources().getDisplayMetrics().density;
-
-		// Convert the dps to pixels, based on density scale.
-		int mGestureThreshold = (int) (valueDip * scale + 0.5f);
-		return mGestureThreshold;
-	}
-
-	/**
-	 * This function used for create bitmap image from view normal.
-	 * */
-	public static Bitmap createImageFromFrags(View v) {
-
-		Bitmap bmp = null;
-		try {
-			if (v != null) {
-				bmp = Bitmap.createBitmap(v.getMeasuredWidth(),
-						v.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
-				Canvas canvas = new Canvas(bmp);
-				v.draw(canvas);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return bmp;
-	}
-
-	/**
-	 * This function used for create bitmap image from view with hide layout.
-	 * */
-	public static Bitmap createBitmapFromView(Activity context, View view) {
-
-		Bitmap b = null;
-		try {
-			if (view != null) {
-
-				DisplayMetrics displayMetrics = new DisplayMetrics();
-				context.getWindowManager().getDefaultDisplay()
-						.getMetrics(displayMetrics);
-				view.measure(MeasureSpec.makeMeasureSpec(
-						displayMetrics.widthPixels, MeasureSpec.EXACTLY),
-						MeasureSpec.makeMeasureSpec(
-								displayMetrics.heightPixels,
-								MeasureSpec.EXACTLY));
-				view.layout(0, 0, displayMetrics.widthPixels,
-						displayMetrics.heightPixels);
-				b = Bitmap.createBitmap(view.getMeasuredWidth(),
-						view.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
-				Canvas canvas = new Canvas(b);
-				view.draw(canvas);
-			}
-		} catch (Exception e) {
-			Log.e(TAG, "Save file error");
-			return null;
-		}
-		return b;
-	}
-
-	public static String saveImageToLocalfile(String fileName, View v) {
-
-		// Create folder if not exist.
-		Bitmap bmp = null;
-		final String pathFolder = Environment.getExternalStorageDirectory()
-				+ File.separator;
-		File f = new File(pathFolder);
-		if (!f.exists()) {
-			f.mkdirs();
-		}
-		String result = "";
-		try {
-
-			// Save output image to file.
-			try {
-				if (v != null) {
-					bmp = Bitmap.createBitmap(v.getMeasuredWidth(),
-							v.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
-					Canvas canvas = new Canvas(bmp);
-					v.draw(canvas);
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			FileOutputStream out = new FileOutputStream(pathFolder + fileName);
-			bmp.compress(Bitmap.CompressFormat.PNG, 100, out);
-			out.close();
-			result = pathFolder + fileName;
-			return result;
-		} catch (IOException e) {
-			return null;
-		} finally {
-			bmp.recycle();
-			bmp = null;
-		}
-	}
 
 }
